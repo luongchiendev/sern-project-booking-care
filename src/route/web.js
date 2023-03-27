35765d30feadb9216276382855026dc5398301f3
SuperEELzx56 <ng.luongchien356@gmail.com>
2023-03-27 23:16:59 +0700

    first commit

---
+++ src/route/web.js
@@ -0,0 +1,14 @@
+import express from "express";
+import homeControllers from "../controllers/homeControllers";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get('/homepage', homeControllers.getHomePage);
+    router.get('/luongchiendev', (req, res) => {
+        return res.send('Hello World, luongchiendev')
+    })
+    return app.use("/", router);
+}
+
+module.exports = initWebRoutes;
