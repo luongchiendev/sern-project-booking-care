b4323f57dc66a6f52f26b99c2be3a585cffc6fc6
SuperEELzx56 <ng.luongchien356@gmail.com>
2023-03-30 22:25:51 +0700

    create form connect to db

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,4 +1,5 @@
 import db from '../models/index';
+import CRUDServices from "../services/CRUDServices"
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -9,6 +10,26 @@ let getHomePage = async (req, res) => {
         console.log(e);
     }
 }
+
+let getAboutPage = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDServices.createNewUser(req.body);
+    console.log(message)
+    return res.send('Post crud from server');
+}
+
+
 module.exports = {
     getHomePage: getHomePage,
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
